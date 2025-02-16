a161d9aa51f78de366ebadc77064ee5ebc4f697c
mingo-inc <laithkaseb99@gmail.com>
2025-02-16 15:31:34 +0100

    added a new scene in the menu

---
+++ Assets/CharacterSelection.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class CharacterSelection : MonoBehaviour
+{
+    public void PlayAsMelee()
+    {
+        SceneManager.LoadScene("PointSmashCollectorScene"); // Replace with your actual scene name
+    }
+}
